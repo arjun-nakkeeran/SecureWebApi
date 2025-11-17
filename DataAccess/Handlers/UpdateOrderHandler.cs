81386bbe92578e4aa2a4fb1d3caedc7d038c465b
Arjun Nakkeeran <Arjun_Nakkeeran@epam.com>
2025-11-17 13:31:21 +0530

    Add Mediator classes for Update order function

---
+++ DataAccess/Handlers/UpdateOrderHandler.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Commands;
+using DataAccess.Models;
+using DataAccess.Services;
+using MediatR;
+using System.Text.Json;
+
+namespace DataAccess.Handlers
+{
+    internal class UpdateOrderHandler : IRequestHandler<UpdateOrderCommand, Guid>
+    {
+        public async Task<Guid> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            Order newOrder = new Order
+            {
+                Id = request.OrderId,
+                CustomerName = request.CustomerName,
+                Items = request.Items,
+            };
+            var eventData = JsonSerializer.Serialize(newOrder);
+            await File.AppendAllLinesAsync(EventStoreConfig.EventStoreFilePath, [eventData], cancellationToken);
+            return newOrder.Id;
+        }
+    }
+}
